20350024a56e9b7bf900a4100a90291f62b8e5d5
Franck Achkouyan <achko.franck@gmail.com>
2016-07-21 02:48:03 +0200

    updated docker file and clean up some code

--- app.js
+++ app.js
@@ -14,12 +14,9 @@ MongoClient.connect(url, function (err, database) {
 });
 
 app.use(bodyparser.json());
-app.use(express.static('public'));
 
 var insertDocument = function (db, document, callback) {
-    // Get the documents collection
     var collection = db.collection('documents');
-    // Insert some documents
     collection.insertOne(document, function (err, result) {
         callback(err, JSON.stringify(result.ops[0]));
     });
@@ -33,15 +30,15 @@ app.post('/hello', function (req, res) {
 });
 
 app.get('/hello', function (req, res) {
-    db.collection('documents').find().toArray(function (err, result) {
+    db.collection('documents').find().toArray(function (err, results) {
       if (err) {
-	res.send(err);
-      } else if (result.length) {
-        res.send(JSON.stringify(result));
+	return res.send(err);
+      } else if (results.length) {
+        return res.send(JSON.stringify(results));
       } else {
-        res.send('No document(s) found');
+        return res.send('No document(s) found');
       }
    });
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 3000);
